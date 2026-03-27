917156fa85d8ad83b18cf1acc6e1a6e82e31aa13
Avaray <1459553+Avaray@users.noreply.github.com>
2026-03-27 15:27:52 +0100

    settings: change default UI font size to 18

--- src/settings.rs
+++ src/settings.rs
@@ -301,7 +301,7 @@ fn default_max_lines() -> usize {
 impl Default for Settings {
     fn default() -> Self {
         Self {
-            ui_font_size: 20.0,
+            ui_font_size: 18.0,
             editor_font_size: 20.0,
             line_height: 1.2,
             ui_font_family: "Proportional (Default)".to_string(),
